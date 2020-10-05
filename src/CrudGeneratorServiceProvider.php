dc07c6ca2dd1073c29286df9afe6867d205be1b3
kevinlabre <kevinlabreckc@gmail.com>
2020-10-05 12:53:47 +0200

    test webhook

--- src/CrudGeneratorServiceProvider.php
+++ src/CrudGeneratorServiceProvider.php
@@ -14,6 +14,7 @@ use KevinLbr\CrudGenerator\Console\Commands\CrudViewIndexCommand;
 use KevinLbr\CrudGenerator\Console\Commands\CrudViewsCommand;
 use Illuminate\Support\ServiceProvider;
 
+
 /**
  * Class CrudGeneratorServiceProvider
  * @package KevinLbr\CrudGenerator
